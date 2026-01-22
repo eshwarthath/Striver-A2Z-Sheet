46aaa5f916715c2e7b4cada9ed865fc3d5dfc44b
Eshwar Thath K R <eshwarthath@gmail.com>
2026-01-22 23:55:12 +0530

    Add prime checking functionality in check_prime.cpp

---
+++ check_prime.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+
+    Solution s;
+    bool result = s.isPrime(n);
+
+    cout << boolalpha << result << endl;  // 👈 prints true/false
+    return 0;
+}
