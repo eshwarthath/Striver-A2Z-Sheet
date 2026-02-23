c77bc528360a1d432ea6216c74a7e701429307b1
Eshwar Thath K R <eshwarthath@gmail.com>
2026-02-23 23:54:40 +0530

    Implement function to find max consecutive ones

---
+++ findMaxConsecutiveOnes_array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = 0;
+        int ones = 0;
+        for(int i = 0; i<n ; i++){
+            if(nums[i] == 1){
+                ones += 1;
+                maxi = max(ones,maxi);
+            }
+            else{
+                ones = 0;
+            }
+        }
+        return maxi;
+        
+    }
+};
