a594ffdeb064763cc8378d5022cbe1eecc5672ad
Eshwar Thath K R <eshwarthath@gmail.com>
2026-03-03 23:51:17 +0530

    Create majorityElement_array.cpp

---
+++ majorityElement_array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int element = 0;
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(count == 0) {
+                element = nums[i];
+            }
+
+            if(nums[i] == element) {
+                count++;
+            } 
+            else {
+                count--;
+            }
+        }
+
+        return element;
+    }
+};
