6eac9270a4573a3c6ec781a109ab5c47ca2df0a3
Eshwar Thath K R <eshwarthath@gmail.com>
2026-09-09 23:40:12 +0530

    Implement string to integer conversion function

---
+++ R_string_to_integer_atoi.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+
+        int i = 0;
+        int n = s.length();
+
+        while(i < n && s[i] == ' ')
+        {
+            i++;
+        }
+
+        int sign = 1;   // Default is positive
+
+        if(i < n && (s[i] == '+' || s[i] == '-'))
+        {
+            if(s[i] == '-')
+            {
+                sign = -1;
+            }
+            i++;
+        }
+
+        long long num = 0;
+
+        while(i < n && isdigit(s[i]))
+        {
+            int digit = s[i] - '0';
+
+            if(sign == 1)
+            {
+                if(num > INT_MAX/10 ||
+                  (num == INT_MAX/10 && digit > 7))
+                {
+                    return INT_MAX;
+                }
+            }
+            else
+            {
+                if(num > INT_MAX/10 ||
+                  (num == INT_MAX/10 && digit > 8))
+                {
+                    return INT_MIN;
+                }
+            }
+
+            num = num * 10 + digit;
+
+            i++;
+        }
+
+        return sign * num;
+    }
+};
