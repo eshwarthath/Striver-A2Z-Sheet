4ac043521f33df2433f072a3e4292c1102a849ae
Eshwar Thath K R <eshwarthath@gmail.com>
2026-09-12 23:20:24 +0530

    Implement countGoodNumbers function in R_count_good_no.cpp

---
+++ R_count_good_no.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long MOD = 1000000007;
+
+    long long power(long long x, long long n) {
+        long long ans = 1;
+
+        while (n > 0) {
+            if (n % 2 == 1) {
+                ans = (ans * x) % MOD;
+            }
+
+            x = (x * x) % MOD;
+            n = n / 2;
+        }
+
+        return ans;
+    }
+
+    int countGoodNumbers(long long n) {
+
+        long long even = (n + 1) / 2;
+        long long odd = n / 2;
+
+        long long evenWays = power(5, even);
+        long long oddWays = power(4, odd);
+
+        return (evenWays * oddWays) % MOD;
+    }
+};
