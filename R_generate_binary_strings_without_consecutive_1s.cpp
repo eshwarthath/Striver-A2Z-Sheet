871e694ce16d352d9fb22abce436b6142379dab7
Eshwar Thath K R <eshwarthath@gmail.com>
2026-09-16 23:30:32 +0530

    Implement binary string generator without consecutive 1s

---
+++ R_generate_binary_strings_without_consecutive_1s.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void generate(int n,string curr, vector<string> &ans){
+        if(curr.length() == n){
+            ans.push_back(curr);
+            return;
+        }
+
+        generate(n,curr+'0',ans);
+        if(curr.empty()|| curr.back()!= '1'){
+            generate(n,curr+'1',ans);
+        }
+
+    }
+
+    vector<string> generateBinaryStrings(int n) {
+        vector<string> ans;
+        generate(n,"",ans);
+        return ans;
+    }
+};
